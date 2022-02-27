95c6cdc8b6695827a37c41d3310a56f3616677d6
Lanay Brown <lanayb.03@gmail.com>
2022-02-27 15:40:38 -0500

    updated db folder, storeNote,js

---
+++ db/storeNote.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const uuidv4 = require('uuid/v4');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+class StoreNote {
+    read(){
+        return readFile('db/db.json', 'utf8');
+    }
+
+    write(note) {
+        return writeFile('db/db.json', JSON.stringify(note));
+    }
+
+    getNotes(){
+        return this.read().then ((notes) => {
+            var parsedNotes;
+
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (error) {
+                parsedNotes = [];
+            }
+
+            return parsedNotes;
+        });
+    }
+
+    newNote(note) {
+        const  { text } = note;
+
+        if(!text){
+            console.log('please add text');
+        }
+
+        const NewNote = { text, id: uuidv4() };
+
+        return this.getNotes()
+            .then((notes) => [...notes, NewNote])
+            .then((updateNote) => this.write(updateNote))
+            .then(() => this.NewNote)
+            .catch((err) => (err));
+
+    }
+
+        // const newNote = { text, id: uuidv4() }
+       
+
+
+}
+
+
+
+module.exports = StoreNote;
+
